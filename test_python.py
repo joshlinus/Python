58943f1669eb540924d6d01dc4e55aaaf53418e9
Joshua Linus <joshualinus1@icloud.com>
2020-08-15 19:30:16 -0500

    Created test.python
    Here is where I will get myself introduced to python. This is just the starting syntax.

---
+++ test_python.py
@@ -0,0 +1,45 @@
+if 5 < 2:
+    print("5 is greater than 2")
+else:
+    print("This is quantum math")
+#this is a comment
+'''
+This 
+is 
+a 
+multi
+lined 
+comment
+'''
+#Creating variables
+x = 5
+y = "5"
+z = "Five"
+
+#Printing Variables
+print (x)
+print (y)
+print (z)
+
+#Changing Variables
+y = "Five"
+print(y)
+
+#String variables can be declared either by using single or double quotes
+v = "John"
+#is the same as
+v = 'John'
+print (v)
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Illegal variable names:
+#2myvar = "John"
+#my-var = "John" #NO DASHES
+#my var = "John"
